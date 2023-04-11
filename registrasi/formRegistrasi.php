8c77629fa32eb2b106db3925f370c2e20e05ea89
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-04-11 19:03:34 +0700

    REGISTRASI

--- registrasi/formRegistrasi.php
+++ registrasi/formRegistrasi.php
@@ -2,48 +2,120 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Registrasi</title>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Form Registrasi</title>
+
+  <!-- Font Google -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+
+  <style>
+    * {
+      font-family: 'Poppins';
+      max-width: 100%;
+      margin: 0 auto;
+    }
+
+    .container {
+      width: 50%;
+      margin: 0 auto;
+    }
+
+    .head {
+      margin-bottom: 40px;
+    }
+
+    form {
+      width: 100%;
+    }
+
+    form input,
+    textarea {
+      width: 97%;
+      padding: 10px 10px;
+      border-radius: 5px;
+      border: 1px solid gray;
+      margin-bottom: 10px;
+    }
+
+    form input[type='radio'] {
+      width: 20px;
+      margin-bottom: 20px;
+    }
+
+    form label {
+      font-weight: 500;
+    }
+
+    form .button {
+      background-color: black;
+      color: white;
+      font-weight: 600;
+      padding: 10px 0;
+      width: 100%;
+      margin-bottom: 10px;
+      border-radius: 5px;
+    }
+  </style>
 </head>
 
 <body>
-    <h1>Form Registrasi</h1>
-    <p>Isi Data Di Bawah Ini :</p>
+  <div class="container">
+    <div class="head">
+      <h1>Form Registrasi</h1>
+      <p>Isi Data Di Bawah Ini</p>
+    </div>
     <form action="tampilRegistrasi.php" method="post">
-        <label for="">Nama</label>
-        <br>
-        <input type="text" name="nama">
-        <br>
-        <label for="">Alamat</label> <br>
-        <textarea name="alamat" id="" cols="50" rows="5"></textarea>
-        <br>
-        <label for="">Tempat Lahir</label> <br>
-        <input type="text" name="tempat_lahir">
-        <br>
-        <label for="">Tanggal Lahir</label> <br>
-        <input type="text" name="tanggal_lahir">
-        <br>
-        <label for="">Jenis Kelamin</label> <br>
-        <input type="radio" id="" name="jenis_kelamin" value="Laki-laki">
-        <label for="">Laki - laki</label>
-        <input type="radio" id="" name="jenis_kelamin" value="Perempuan">
-        <label for="">Perempuan</label><br>
-        <br>
-        <label for="">Pendidikan</label>
+      <!-- Form Nama -->
+      <label for="Nama">Nama</label>
+      <br />
+      <input type="text" name="nama" placeholder="Masukkan Nama Anda" />
+      <br />
 
-        <select id="" name="pendidikan">
-            <option value="SMA">SMA</option>
-            <option value="D3">D3</option>
-            <option value="S1">S1</option>
-            <option value="S2">S2</option>
-        </select>
-        <br>
-        <br>
-        <input type="submit" value="Submit" class="button">
-        <input type="reset" value="Reset" class="button">
+      <!-- Form Alamat -->
+      <label for="Alamat">Alamat</label> <br />
+      <textarea name="alamat" id="" cols="20" rows="5" placeholder="Masukkan Alamat Lengkap Anda"></textarea>
+      <br />
+
+      <!-- Form Tempat Lahir -->
+      <label for="Tempat Lahir">Tempat Lahir</label> <br />
+      <input type="text" name="tempat_lahir" placeholder="Masukkan Tempat Lahir Anda" />
+      <br />
+
+      <!-- Form Tanggal Lahir -->
+      <label for="Tanggal Lahir">Tanggal Lahir</label> <br />
+      <input type="text" name="tanggal_lahir" placeholder="Masukkan Tanggal Lahir Anda" />
+      <br />
+
+      <!-- Form Jenis Kelamin -->
+      <label for="Jenis Kelamin">Jenis Kelamin</label>
+      <label for="Laki-laki">
+        <input type="radio" name="jenis_kelamin" value="Laki-laki" />
+        Laki - laki
+      </label>
+      <label for="Perempuan">
+        <input type="radio" name="jenis_kelamin" value="Perempuan" />
+        Perempuan
+      </label>
+      <br />
+
+      <!-- Form Pendidikan -->
+      <label for="Pendidikan">Pendidikan</label>
+      <select name="pendidikan">
+        <option value="SMA">SMA</option>
+        <option value="D3">D3</option>
+        <option value="S1">S1</option>
+        <option value="S2">S2</option>
+      </select>
+      <br />
+      <br />
+      <button type="submit" value="Submit" class="button">Submit </button>
+      <button type="reset" value="Reset" class="button">Reset </button>
     </form>
+  </div>
 </body>
 
 </html>
--- registrasi/tampilRegistrasi.php
+++ registrasi/tampilRegistrasi.php
@@ -7,38 +7,72 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Registrasi</title>
+
+    <!-- Font Google -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+
+
+    <style>
+        * {
+            font-family: 'Poppins';
+            max-width: 100%;
+            margin: 10px auto;
+        }
+
+        .container button {
+            padding: 10px 30px;
+            background-color: black;
+            border: none;
+            margin-top: 10px;
+            border-radius: 5px;
+        }
+
+        .container button a {
+            color: white;
+            text-decoration: none;
+            font-weight: 600;
+        }
+
+        .container table td {
+            padding: 0 5px;
+        }
+    </style>
 </head>
 
 
 <body>
-    <h1>Data Registrasi</h1>
-    <table style="width:30%" border="1">
-        <tr>
-            <td>Nama</td>
-            <td><?php echo $_POST['nama'] ?></td>
-        </tr>
-        <tr>
-            <td>Alamat</td>
-            <td><?php echo $_POST['alamat'] ?></td>
-        </tr>
-        <tr>
-            <td>Tempat Lahir</td>
-            <td><?php echo $_POST['tempat_lahir'] ?></td>
-        </tr>
-        <tr>
-            <td>Tanggal Lahir</td>
-            <td><?php echo $_POST['tanggal_lahir'] ?></td>
-        </tr>
-        <tr>
-            <td>Jenis Kelamin</td>
-            <td><?php echo $_POST['jenis_kelamin'] ?></td>
-        </tr>
-        <tr>
-            <td>Pendidikan</td>
-            <td><?php echo $_POST['pendidikan'] ?></td>
-        </tr>
-    </table>
-    <a href="formRegistrasi.php">Input Data lagi</a>
+    <div class="container" align="center">
+        <h1>Data Registrasi</h1>
+        <table style="width:30%" border="1">
+            <tr>
+                <td>Nama</td>
+                <td><?php echo $_POST['nama'] ?></td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td><?php echo $_POST['alamat'] ?></td>
+            </tr>
+            <tr>
+                <td>Tempat Lahir</td>
+                <td><?php echo $_POST['tempat_lahir'] ?></td>
+            </tr>
+            <tr>
+                <td>Tanggal Lahir</td>
+                <td><?php echo $_POST['tanggal_lahir'] ?></td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td><?php echo $_POST['jenis_kelamin'] ?></td>
+            </tr>
+            <tr>
+                <td>Pendidikan</td>
+                <td><?php echo $_POST['pendidikan'] ?></td>
+            </tr>
+        </table>
+        <button> <a href="formRegistrasi.php">Input Data lagi</a></button>
+    </div>
 </body>
 
 </html>
